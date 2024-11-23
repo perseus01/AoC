9e3ec80d8f2811c9f140eb815f4acd9e8e1a224c
Divyang <8656786+perseus01@users.noreply.github.com>
2024-11-23 16:32:55 -0500

    feat!: add AoC 2015, Day 4, Part 2

---
+++ 2015/4/4-2.py
@@ -0,0 +1,15 @@
+import hashlib
+
+key = "bgvyzdsv"
+flag = 0
+step = 1
+
+while flag == 0:
+    result = hashlib.md5(f"{key}{step}".encode())
+    hex_result = result.hexdigest()
+    if hex_result[:6] == "000000":
+        flag = 1
+    else:
+        step += 1
+
+print(f"Lowest positive number: {step}")
