92225ef21dd403336b451e72d32d7e7c56ea4f83
Divyang <8656786+perseus01@users.noreply.github.com>
2024-11-23 13:21:07 -0500

    feat!: Add AoC 2015, Day 3, Part 1 and Day 3 input

---
+++ 2015/3/3-1.py
@@ -0,0 +1,23 @@
+with open("3_input.txt", "r") as f:
+    inp = f.read()
+    houses = [(0, 0)]
+    step = 1
+    for direction in inp:
+        current_location = houses[-1]
+
+        if direction == "<":
+            new_location = (current_location[0] - 1, current_location[1])
+
+        elif direction == ">":
+            new_location = (current_location[0] + 1, current_location[1])
+
+        elif direction == "^":
+            new_location = (current_location[0], current_location[1] + 1)
+
+        elif direction == "v":
+            new_location = (current_location[0], current_location[1] - 1)
+
+        houses.append(new_location)
+
+    houses = set(houses)
+    print(f"Total houses visited: {len(houses)}")
